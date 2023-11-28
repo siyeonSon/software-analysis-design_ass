b2cceb656cce9bd2a6ae146e560da2d0b2ab2767
siyeonSon <siyeon0209@naver.com>
2023-11-28 15:26:16 +0900

    feat: solve test07

--- src/problem4/CheckingBuilderProxyDecorator.java
+++ src/problem4/CheckingBuilderProxyDecorator.java
@@ -20,7 +20,30 @@ public class CheckingBuilderProxyDecorator extends BaseBuilderProxyDecorator {
 
     @Override
     public Car getCar() {
+        if (isDoorInRange() && isWheelInRange() && isEvenWheel() && isSingleRoof()) {
+            return car;
+        }
         return null;
+    }
+
+    private boolean isDoorInRange() {
+        int numDoors = car.numDoors();
+        return 4 <= numDoors && numDoors <= 6;
+    }
+
+    private boolean isWheelInRange() {
+        int numWheels = car.numWheels();
+        return 2 <= numWheels && numWheels <= 10;
+    }
+
+    private boolean isEvenWheel() {
+        int numWheels = car.numWheels();
+        return numWheels % 2 == 0;
+    }
+
+    private boolean isSingleRoof() {
+        int numRoofs = car.numRoofs();
+        return numRoofs == 1;
     }
 }
 /**
